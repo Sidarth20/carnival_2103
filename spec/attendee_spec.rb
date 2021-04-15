9661d42babeae671e00c893a4d61e580a1983c75
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-15 09:13:08 -0600

    Finish iteration 1

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Attendee do
     expect(attendee.spending_money).to eq(20)
     expect(attendee.interests).to eq([])
   end
+
+  it 'adds interests'do
+    attendee = Attendee.new('Bob', 20)
+
+    attendee.add_interest('Bumper Cars')
+    attendee.add_interest('Ferris Wheel')
+    expect(attendee.interests).to eq(["Bumper Cars", "Ferris Wheel"])
+  end
 end
