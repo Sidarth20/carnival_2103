21beb2352d3354125a2bc3f4086dc548b7ebce05
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-15 10:24:07 -0600

    Attempt at recommend rides

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,4 +9,14 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  #need to return an array of ride objects
+  # if attendee interests array of strings == ride.name
+  def recommend_rides(attendee)
+    match = attendee.interests.find_all do |interest|
+      @rides.flat_map do |ride|
+        interest == ride.name
+      end
+    end
+  end
 end
